815b741ecc0309cefa1e0c0cfddbf16ced9267ea
Ali-Raza-J <89023546+AliRaza0423@users.noreply.github.com>
2021-11-03 18:06:02 +0500

    updated templated

--- lib/Screens/screen2.dart
+++ lib/Screens/screen2.dart
@@ -13,10 +13,9 @@ class Screen2 extends StatelessWidget {
           color: Colors.white,
         ),
         child: StaggeredGridView.countBuilder(
-          
-          itemCount: _totalcount % 21 == 0 
-          ? _totalcount + _totalcount~/21
-          : _totalcount + _totalcount~/21 + 1,
+          itemCount: _totalcount % 21 == 0
+              ? _totalcount + _totalcount ~/ 21
+              : _totalcount + _totalcount ~/ 21 + 1,
           // myproduct2.length + myBanar2.length + myText2.length +2,
           crossAxisCount: 12,
           // crossAxisSpacing: 10,
@@ -46,7 +45,7 @@ class Screen2 extends StatelessWidget {
           itemBuilder: (context, index) {
             // model = myproduct2[index];
             return index % 22 == 0 || index % 22 == 10 || index % 22 == 17
-                ? ImageTile1(
+                ? imageTile1(
                     image: myBanar2[((index / 22) * 4 - (index / 22)).toInt()]
                         .image,
                     text1: myBanar2[((index / 22) * 4 - (index / 22)).toInt()]
@@ -74,7 +73,7 @@ class Screen2 extends StatelessWidget {
                         ),
                       )
                     : index % 22 == 1 && index != 1
-                        ? const SizedBox(height: 5)
+                        ? const SizedBox(height: 1)
                         : index % 22 == 14 //|| index % 22 == 20
                             ? textTile(
                                 text1: myText2[(index ~/ 22) * 2].text1,
@@ -88,7 +87,7 @@ class Screen2 extends StatelessWidget {
                                         myText2[((index ~/ 22) * 2) + 1].text2,
                                   )
                                 : index % 22 >= 2 && index % 22 <= 9
-                                    ? ImageTile2(
+                                    ? imageTile2(
                                         image: myproduct2[(index ~/ 22) * 16 +
                                                 ((index % 22) - 2)]
                                             .image, //'assets/images/2.png',
@@ -99,8 +98,8 @@ class Screen2 extends StatelessWidget {
                                                 ((index % 22) - 2)]
                                             .text2, //'PKR 4,990.00',
                                       )
-                                    : index % 11 >= 2 && index % 22 <= 13
-                                        ? ImageTile2(
+                                    : index % 22 >= 11 && index % 22 <= 13
+                                        ? imageTile2(
                                             image: myproduct2[
                                                     (index ~/ 22) * 16 +
                                                         ((index % 22) - 3)]
@@ -115,7 +114,7 @@ class Screen2 extends StatelessWidget {
                                                 .text2, //'PKR 4,990.00',
                                           )
                                         : index % 22 == 15 || index % 22 == 16
-                                            ? ImageTile2(
+                                            ? imageTile2(
                                                 image: myproduct2[
                                                         (index ~/ 22) * 16 +
                                                             ((index % 22) - 4)]
@@ -131,7 +130,7 @@ class Screen2 extends StatelessWidget {
                                               )
                                             : index % 22 == 18 ||
                                                     index % 22 == 19
-                                                ? ImageTile2(
+                                                ? imageTile2(
                                                     image: myproduct2[
                                                             (index ~/ 22) * 16 +
                                                                 ((index % 22) -
@@ -148,7 +147,7 @@ class Screen2 extends StatelessWidget {
                                                                     5)]
                                                         .text2, //'PKR 4,990.00',
                                                   )
-                                                : ImageTile2(
+                                                : imageTile2(
                                                     image: myproduct2[
                                                             (index ~/ 22) * 16 +
                                                                 ((index % 22) -
@@ -170,124 +169,103 @@ class Screen2 extends StatelessWidget {
       ),
     );
   }
-}
-
-class textTile extends StatelessWidget {
-  textTile({
-    this.text1,
-    this.text2,
-  });
-  String? text1;
-  String? text2;
-
-  @override
-  Widget build(BuildContext context) {
+//---------------------------TextTile-----------------------
+  Container textTile({String? text1,
+  String? text2}) {
     return Container(
-      // margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            text1!,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-          ),
-          const SizedBox(height: 5),
-          Text(
-            text2!,
-          ),
-        ],
-      ),
-    );
+    // margin: const EdgeInsets.all(10),
+    padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text1!,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+        ),
+        const SizedBox(height: 5),
+        Text(
+          text2!,
+        ),
+      ],
+    ),
+  );
   }
-}
 
-class ImageTile1 extends StatelessWidget {
-  ImageTile1({
-    this.image,
-    this.text1,
-    this.text2,
-  });
-  String? image;
-  String? text1;
-  String? text2;
-
-  @override
-  Widget build(BuildContext context) {
+  //---------------------------Imagetile1------------------------------
+  Container imageTile1({String? image,
+  String? text1,
+  String? text2}) {
     return Container(
-      // margin: const EdgeInsets.symmetric(horizontal: 40),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          SizedBox(
-            height: 280,
-            // width: double.infinity,
-            child: Image.asset(
-              image!,
-              fit: BoxFit.fill,
-            ),
+    // margin: const EdgeInsets.symmetric(horizontal: 40),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        SizedBox(
+          height: 280,
+          // width: double.infinity,
+          child: Image.asset(
+            image!,
+            fit: BoxFit.fill,
           ),
-          Material(
-            color: Colors.transparent,
-            child: InkWell(
-              highlightColor: Colors.grey[300]!.withOpacity(0.3),
-              splashColor: Colors.grey[300]!.withOpacity(0.3),
-              onTap: () {},
-            ),
+        ),
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            highlightColor: Colors.grey[300]!.withOpacity(0.3),
+            splashColor: Colors.grey[300]!.withOpacity(0.3),
+            onTap: () {},
           ),
-          Text(text2!),
-        ],
-      ),
-    );
+        ),
+        Text(text2!),
+      ],
+    ),
+  );
   }
-}
-
-class ImageTile2 extends StatelessWidget {
-  ImageTile2({this.image, this.text1, this.text2});
-  String? image;
-  String? text1;
-  String? text2;
-  @override
-  Widget build(BuildContext context) {
+  //------------------------------ImageTile2--------------------------
+  Container imageTile2({String? image,
+  String? text1,
+  String? text2}) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 3),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Column(
-            children: [
-              SizedBox(
-                height: 260,
-                // width: double.infinity,
-                child: Image.asset(
-                  image!,
-                  fit: BoxFit.fill,
-                ),
+    padding: const EdgeInsets.symmetric(horizontal: 3),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Column(
+          children: [
+            SizedBox(
+              height: 260,
+              // width: double.infinity,
+              child: Image.asset(
+                image!,
+                fit: BoxFit.fill,
               ),
-              const SizedBox(height: 12),
-              Text(text1!, style: const TextStyle(fontSize: 12)),
-              Text(
-                text2!,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
-                ),
-              ),
-            ],
-          ),
-          Material(
-            color: Colors.transparent,
-            child: InkWell(
-              highlightColor: Colors.grey[300]!.withOpacity(0.3),
-              splashColor: Colors.grey[300]!.withOpacity(0.3),
-              onTap: () {},
             ),
+            const SizedBox(height: 12),
+            Text(text1!, style: const TextStyle(fontSize: 12)),
+            Text(
+              text2!,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            highlightColor: Colors.grey[300]!.withOpacity(0.3),
+            splashColor: Colors.grey[300]!.withOpacity(0.3),
+            onTap: () {},
           ),
-        ],
-      ),
-    );
+        ),
+      ],
+    ),
+  );
   }
+
 }
+
 
 class MyProduct2 {
   String? image;
@@ -326,45 +304,45 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 1',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 2',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
-  ),
-  //------------------------------
-  MyProduct2(
-    image: 'assets/images/2.png',
-    text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 3',
   ),
   //------------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 4',
+  ),
+  //------------------------------
+  MyProduct2(
+    image: 'assets/images/2.png',
+    text1: 'ROZANA SUITS',
+    text2: 'PKR 4,990. 5',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 6 ',
   ),
   //--------------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 7',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 8',
   ),
   // //-----------------------------------
   // MyProduct2(
@@ -376,18 +354,18 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 9',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 10',
   ),
   //--------------------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 11',
   ),
   //----------------------------------
   // MyProduct2(
@@ -398,12 +376,12 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 12',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 13',
   ),
   //------------------------------------
   // MyProduct2(
@@ -415,12 +393,12 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 14',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 15',
   ),
   // MyProduct2(
   //   text1: 'Lorem ipsum dollar sit',
@@ -431,52 +409,52 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 16',
   ),
 
   //--------------------------second list----------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 17',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 18',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
-  ),
-  //------------------------------
-  MyProduct2(
-    image: 'assets/images/2.png',
-    text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 19',
   ),
   //------------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 20',
+  ),
+  //------------------------------
+  MyProduct2(
+    image: 'assets/images/2.png',
+    text1: 'ROZANA SUITS',
+    text2: 'PKR 4,990.00 21',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 22',
   ),
   //--------------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 23',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 24',
   ),
   // //-----------------------------------
   // MyProduct2(
@@ -488,18 +466,18 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 25',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 26',
   ),
   //--------------------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 27',
   ),
   //----------------------------------
   // MyProduct2(
@@ -510,12 +488,12 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 28',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 29',
   ),
   //------------------------------------
   // MyProduct2(
@@ -527,12 +505,12 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 30',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 31',
   ),
   // MyProduct2(
   //   text1: 'Lorem ipsum dollar sit',
@@ -543,29 +521,29 @@ List<MyProduct2> myproduct2 = [
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 32',
   ),
   //--------------------------third list----------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 33',
   ),
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 34',
   ),
-  MyProduct2( 
+  MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 35',
   ),
   //------------------------------
   MyProduct2(
     image: 'assets/images/2.png',
     text1: 'ROZANA SUITS',
-    text2: 'PKR 4,990.00',
+    text2: 'PKR 4,990.00 36',
   ),
 ];
 
